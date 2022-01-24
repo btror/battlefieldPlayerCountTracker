cc1dde83ca66b542581ddd2648de6931028acff2
btror <ticer1999@gmail.com>
2022-01-24 02:17:12 -0500

    made subplots stacked

--- GameTracker.py
+++ GameTracker.py
@@ -7,7 +7,7 @@ class GameTracker:
         self.data = data
         self.titles = titles
         self.largest_game_data = max(len(x) for x in self.data)
-        self.fig, self.axs = plt.subplots(2, 2)
+        self.fig, self.axs = plt.subplots(3)
         self.setup_graph()
 
     def setup_graph(self):
@@ -27,18 +27,18 @@ class GameTracker:
 
         if bf_title_index != -1:
             first_battlefield_steam_data_index = self.largest_game_data - len(self.data[bf_title_index])
-            self.axs[0, 0].axvline(x=first_battlefield_steam_data_index, color="k", linestyle=":", label="Earliest "
+            self.axs[0].axvline(x=first_battlefield_steam_data_index, color="k", linestyle=":", label="Earliest "
                                                                                                          "Battlefield "
                                                                                                          "Steam Data")
-            self.axs[0, 1].axvline(x=first_battlefield_steam_data_index, color="k", linestyle=":", label="Earliest "
+            self.axs[1].axvline(x=first_battlefield_steam_data_index, color="k", linestyle=":", label="Earliest "
                                                                                                          "Battlefield "
                                                                                                          "Steam Data")
-            self.axs[1, 0].axvline(x=first_battlefield_steam_data_index, color="k", linestyle=":", label="Earliest "
+            self.axs[2].axvline(x=first_battlefield_steam_data_index, color="k", linestyle=":", label="Earliest "
                                                                                                          "Battlefield "
                                                                                                          "Steam Data")
-            self.axs[1, 1].axvline(x=first_battlefield_steam_data_index, color="k", linestyle=":", label="Earliest "
-                                                                                                         "Battlefield "
-                                                                                                         "Steam Data")
+            # self.axs[3].axvline(x=first_battlefield_steam_data_index, color="k", linestyle=":", label="Earliest "
+            #                                                                                              "Battlefield "
+            #                                                                                              "Steam Data")
 
         bf_2042_title_index = -1
         try:
@@ -49,23 +49,23 @@ class GameTracker:
         if bf_2042_title_index != -1:
             bf2042_title_index = self.titles.index("Battlefield™ 2042")
             first_battlefield2042_steam_data_index = self.largest_game_data - len(self.data[bf2042_title_index])
-            self.axs[0, 0].axvline(x=first_battlefield2042_steam_data_index, color="#40dfbd", linestyle=":",
+            self.axs[0].axvline(x=first_battlefield2042_steam_data_index, color="#40dfbd", linestyle=":",
                                    label="Battlefield "
                                          "2042 Release")
-            self.axs[0, 1].axvline(x=first_battlefield2042_steam_data_index, color="#40dfbd", linestyle=":",
+            self.axs[1].axvline(x=first_battlefield2042_steam_data_index, color="#40dfbd", linestyle=":",
                                    label="Battlefield "
                                          "2042 Release")
-            self.axs[1, 0].axvline(x=first_battlefield2042_steam_data_index, color="#40dfbd", linestyle=":",
+            self.axs[2].axvline(x=first_battlefield2042_steam_data_index, color="#40dfbd", linestyle=":",
                                    label="Battlefield "
                                          "2042 Release")
-            self.axs[1, 1].axvline(x=first_battlefield2042_steam_data_index, color="#40dfbd", linestyle=":",
-                                   label="Battlefield "
-                                         "2042 Release")
+            # self.axs[1, 1].axvline(x=first_battlefield2042_steam_data_index, color="#40dfbd", linestyle=":",
+            #                        label="Battlefield "
+            #                              "2042 Release")
 
-        self.axs[0, 0].axhline(y=0, color="0.85", linestyle="-")
-        self.axs[0, 1].axhline(y=0, color="0.85", linestyle="-")
-        self.axs[1, 0].axhline(y=0, color="0.85", linestyle="-")
-        self.axs[1, 1].axhline(y=0, color="0.85", linestyle="-")
+        self.axs[0].axhline(y=0, color="0.85", linestyle="-")
+        self.axs[1].axhline(y=0, color="0.85", linestyle="-")
+        self.axs[2].axhline(y=0, color="0.85", linestyle="-")
+        # self.axs[1, 1].axhline(y=0, color="0.85", linestyle="-")
 
         for game in self.data:
             for i in range(self.largest_game_data - len(game)):
@@ -95,15 +95,16 @@ class GameTracker:
             }
 
             df = pd.DataFrame(game, columns=["Month", "Avg. Player Count"])
-            self.axs[0, 0].set_xticks(df["Month"], month_name, rotation=80, fontsize=6)
+            self.axs[0].set_xticks(df["Month"], month_name, rotation=80, fontsize=6)
             # self.axs[0, 0].subplots_adjust(bottom=0.3)
-            self.axs[0, 0].set_xlabel("30 Day Period")
-            self.axs[0, 0].set_ylabel("Avg. Player Count")
-            self.axs[0, 0].set_title("Avg. Player Count")
-            self.axs[0, 0].plot(df["Month"], df["Avg. Player Count"], label=self.titles[count])
+            self.axs[0].set_xlabel("30 Day Period")
+            self.axs[0].set_ylabel("Avg. Player Count")
+            self.axs[0].set_title("Avg. Player Count")
+            self.axs[0].plot(df["Month"], df["Avg. Player Count"], label=self.titles[count])
             # self.axs[0, 0].tight_layout()
             count += 1
 
+        self.axs[0].legend()
         # plt.legend()
         # plt.show()
 
@@ -128,15 +129,16 @@ class GameTracker:
             }
 
             df = pd.DataFrame(game, columns=["Month", "Peak Players"])
-            self.axs[0, 1].set_xticks(df["Month"], month_name, rotation=80, fontsize=6)
+            self.axs[1].set_xticks(df["Month"], month_name, rotation=80, fontsize=6)
             # self.axs[0, 1].subplots_adjust(bottom=0.3)
-            self.axs[0, 1].set_xlabel("30 Day Period")
-            self.axs[0, 1].set_ylabel("Peak Players")
-            self.axs[0, 1].set_title("Peak Player Count")
-            self.axs[0, 1].plot(df["Month"], df["Peak Players"], label=self.titles[count])
+            self.axs[1].set_xlabel("30 Day Period")
+            self.axs[1].set_ylabel("Peak Players")
+            self.axs[1].set_title("Peak Player Count")
+            self.axs[1].plot(df["Month"], df["Peak Players"], label=self.titles[count])
             # self.axs[0, 1].tight_layout()
             count += 1
 
+        self.axs[1].legend()
         # plt.legend()
         # plt.show()
 
@@ -170,19 +172,15 @@ class GameTracker:
             }
 
             df = pd.DataFrame(game, columns=["Month", "Avg. Players Gained"])
-            self.axs[1, 0].set_xticks(df["Month"], month_name, rotation=80, fontsize=6)
+            self.axs[2].set_xticks(df["Month"], month_name, rotation=80, fontsize=6)
             # self.axs[1, 0].subplots_adjust(bottom=0.3)
-            self.axs[1, 0].set_xlabel("30 Day Period")
-            self.axs[1, 0].set_ylabel("Avg. Players Gained")
-            self.axs[1, 0].set_title("Avg. Players Gained")
-            self.axs[1, 0].plot(df["Month"], df["Avg. Players Gained"], label=self.titles[count])
+            self.axs[2].set_xlabel("30 Day Period")
+            self.axs[2].set_ylabel("Avg. Players Gained")
+            self.axs[2].set_title("Avg. Players Gained")
+            self.axs[2].plot(df["Month"], df["Avg. Players Gained"], label=self.titles[count])
             # self.axs[1, 0].tight_layout()
             count += 1
 
-        lines_labels = [ax.get_legend_handles_labels() for ax in self.fig.axes]
-        lines, labels = [sum(lol, []) for lol in zip(*lines_labels)]
-
-        self.fig.legend(lines, labels)
-        # self.axs[1, 1].legend()
+        self.axs[2].legend()
         # plt.legend()
         # plt.show()
